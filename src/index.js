fb7488b6b0a2e581a8c4978174da9d7017fd0ec1
mecozma <mecozma@gmail.com>
2019-02-13 21:08:19 +0000

    Added Interceptors to execute code globally

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+  console.log('Interceptor request',request);
+  return request;
+}, error => {
+  console.log("Interceptor", error);
+  return Promise.reject(error)
+});
+
+axios.interceptors.response.use(response => {
+  console.log('Interceptor response', response);
+  return response;
+}, error => {
+  return Promise.reject(error);
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
