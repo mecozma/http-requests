2e2842654bd70c2e317345bfedf6e1fc46a235e2
mecozma <mecozma@gmail.com>
2019-02-13 17:09:36 +0000

    Transforming the data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -13,14 +13,22 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
-                console.log(response);
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'A mighty little man'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+                console.log(updatedPosts);
             });
     }
     render () {
         const posts = this.state.posts.map(post => {
             return(
                 <Post
+                author={post.author}
                     title={post.title}
                     key={post.id} />
             )
