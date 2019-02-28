3cccc0185a159c2b9627c3f6e5cdcaa5533d7da2
mecozma <mecozma@gmail.com>
2019-02-28 22:58:39 +0000

    Navigating Programatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import  './Posts.css';
@@ -33,7 +33,9 @@ class Posts extends Component {
 }
 
   postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id});
+    // this.setState({selectedPostId: id});
+    // this.props.history.push('/' + id);
+    this.props.history.push({pathname: '/' + id});
   }
 
   render() {
@@ -42,12 +44,13 @@ class Posts extends Component {
       if (!this.state.error) {
           posts = this.state.posts.map(post => {
               return (
-                  <Link to={'/' + post.id} key={post.id}>
+                  // <Link to={'/' + post.id} key={post.id}>
                       <Post
+                          key={post.id}
                           author={post.author}
                           title={post.title}
                           clicked={() => this.postSelectedHandler(post.id)} />
-                  </Link>
+                  // </Link>
               );
           })
       }
